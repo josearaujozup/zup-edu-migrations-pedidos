90c99f9948249a80f7a80475930c1f84c1206124
Denes <jose.araujo@zup.com.br>
2022-05-10 16:03:41 -0300

    commit inicial

---
+++ src/test/java/br/com/zup/edu/migrationpedidos/MigrationpedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.migrationpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationpedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
